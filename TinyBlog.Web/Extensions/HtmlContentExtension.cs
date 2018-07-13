0fa244ba92468dcbdda580904e69ff06a1dc09cf
Kevin Keyt <kevinkeyt@gmail.com>
2018-07-12 21:35:52 -0400

    #12 - Update to add upload suport on the post edit page. Also added a helper function to process the html content.

---
+++ TinyBlog.Web/Extensions/HtmlContentExtension.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Text.RegularExpressions;
+using TinyBlog.Domain;
+
+namespace Microsoft.AspNetCore.Mvc.Rendering
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlContent ProcessHtml(this IHtmlHelper htmlHelper, Blog blog, string content)
+        {
+            string result = content;
+
+            // Youtube content embedded using this syntax: [youtube:xyzAbc123]
+            var video = "<div class=\"video\"><iframe src=\"//www.youtube.com/embed/{0}?modestbranding=1&amp;theme=light\" allowfullscreen></iframe></div>";
+            result = Regex.Replace(result, @"\[youtube:(.*?)\]", (Match m) => string.Format(video, m.Groups[1].Value));
+
+            // Images replaced by CDN paths if they are located in the /images/ folder
+            var cdn = blog.CdnUrl;
+            var root = "/images/";
+
+            if (!root.StartsWith("/"))
+                root = "/" + root;
+
+            result = Regex.Replace(result, "<img.*?src=\"([^\"]+)\"", (Match m) =>
+            {
+                string src = m.Groups[1].Value;
+                int index = src.IndexOf(root);
+
+                if (index > -1)
+                {
+                    string clean = src.Substring(index);
+                    return m.Value.Replace(src, cdn + clean);
+                }
+
+                return m.Value;
+            });
+
+            return new HtmlString(result);
+        }
+    }
+}
